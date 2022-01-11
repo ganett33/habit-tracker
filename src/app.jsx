73f3566ff4c3eac87ceef1736d4c3b7a893d400c
sid <ganett33@gmail.com>
2022-01-11 13:21:45 +1300

    Revert "ch4-5"
    This reverts commit ea83c78111a08283bdbf583859e41d2a305b74e5.

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,9 @@
-import React from 'react';
 import './app.css';
 
 function App() {
-  const user ='sidney';
-  const name = undefined;
-  return (
-    // <React.Fragment></React.Fragment></React.Fragment> 
-    <> 
-      <h1 className='title' onClick="" >Hello {user} :)</h1>
-      {user && <h1>Hello! {user} :) </h1>}
-      {user && <h1>Hello! {name} :) </h1>}
-      {
-        ['💚','💤'].map(item => <h2>{item}</h2>)
-      }
-    </>  
+  return ( 
+  <h1>Hello :)</h1>
   );
 }
 
 export default App;
- 
