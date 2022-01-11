ea83c78111a08283bdbf583859e41d2a305b74e5
sid <ganett33@gmail.com>
2022-01-11 13:20:25 +1300

    ch4-5

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,21 @@
+import React from 'react';
 import './app.css';
 
 function App() {
-  return ( 
-  <h1>Hello :)</h1>
+  const user ='sidney';
+  const name = undefined;
+  return (
+    // <React.Fragment></React.Fragment></React.Fragment> 
+    <> 
+      <h1 className='title' onClick="" >Hello {user} :)</h1>
+      {user && <h1>Hello! {user} :) </h1>}
+      {user && <h1>Hello! {name} :) </h1>}
+      {
+        ['💚','💤'].map(item => <h2>{item}</h2>)
+      }
+    </>  
   );
 }
 
 export default App;
+ 
---
+++ src/components/habit.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Habit extends Component {
+    render() {
+        return (
+            <div>Habit</div>
+        )
+    }
+}
+
+export default Habit;
